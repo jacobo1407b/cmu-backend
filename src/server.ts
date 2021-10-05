b3867c34cab0cefea67a3b4b2d27a910d8b02a3e
jacobo1407b <jacobohernandezmendieta7@gmail.com>
2021-10-04 22:18:50 -0500

    create basic server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log('Hola mundo')
+import express from "express";
+import chalk from 'chalk';
+
+const app = express();
+
+
+//settings
+app.set('port', 3000);
+app.set('json spaces', 2);
+//middlewares
+
+//rutas
+
+//err
+
+
+app.listen(app.get('port'), () => {
+    console.log(chalk.blue('INFO: ')+chalk.green(`Server started at http://localhost:${app.get('port')}`))
+});
